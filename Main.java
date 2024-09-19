78b3789cc0509e541426db04f3f9c7ad8cdc6217
nessa29 <vanessay10@nycstudents.net>
2024-09-19 16:34:12 +0000

    first commit

---
+++ Main.java
@@ -0,0 +1,18 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Feeder f = new Feeder();
+        System.out.println(f);
+        //System.out.println(f.currentFood);
+        System.out.println(f.getCurrentFood());
+        Feeder g = new Feeder(500);
+        System.out.println(g.getCurrentFood());
+        g.simulateOneDay(12);
+        System.out.println(Math.random());
+        double random = (int) (Math.random() * 10+1);
+        System.out.println(random);
+    }
+    
+}
+
